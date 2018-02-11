dfc44e1a0f7b71b16fc858188755978880dc4298
zosyk <alekcandrzosyk@gmail.com>
2018-02-11 09:53:04 +0200

    initial commit for Boggle

---
+++ src/BoggleBoard.java
@@ -0,0 +1,37 @@
+public class BoggleBoard {
+    // Initializes a random 4-by-4 Boggle board.
+    // (by rolling the Hasbro dice)
+    public BoggleBoard() {
+    }
+
+    // Initializes a random m-by-n Boggle board.
+    // (using the frequency of letters in the English language)
+    public BoggleBoard(int m, int n) {
+    }
+
+    // Initializes a Boggle board from the specified filename.
+    public BoggleBoard(String filename) {
+    }
+
+    // Initializes a Boggle board from the 2d char array.
+    // (with 'Q' representing the two-letter sequence "Qu")
+    public BoggleBoard(char[][] a) {
+    }
+
+    // Returns the number of rows.
+    public int rows() {
+    }
+
+    // Returns the number of columns.
+    public int cols() {
+    }
+
+    // Returns the letter in row i and column j.
+    // (with 'Q' representing the two-letter sequence "Qu")
+    public char getLetter(int i, int j) {
+    }
+
+    // Returns a string representation of the board.
+    public String toString() {
+    }
+}
